5d558a1932d6b2e1b7272f90d144695bb12fea64
nattaphonsss <49384982+nattaphonsss@users.noreply.github.com>
2019-04-08 14:24:34 +0700

    Initial commit

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $faker = \Faker\Factory::create();
+       for ($i = 0;$i <50;$i++)
+            {
+                Product::create([
+                    'name' => $faker->title,
+                    'price'=>$faker->randomNumber(2),
+                    'num'=>$faker->randomNumber(5)
+                ]);
+            } //
+    }
+}
